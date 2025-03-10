1aaf5a5ed5a27402707db2bed9b7e8a1b093ee2b
Bhavya Bahl <bbahl@google.com>
2025-03-10 23:50:36 +0000

    Update mixtral pytorch batch size with new docker

--- training/trillium/Mixtral-8x7B-Pytorch/XPK/env.sh
+++ training/trillium/Mixtral-8x7B-Pytorch/XPK/env.sh
@@ -8,10 +8,10 @@ export NUM_SLICE=1
 export CLUSTER_NAME=xpk-$USER-... # use existing CLUSTER if you have
 
 # Environment variables associated with training config.
-export BATCH_PER_DEVICE=4
+export BATCH_PER_DEVICE=8
 export SEQUENCE_LENGTH=4096
 export MAX_STEP=50
 export WORKLOAD_NAME=${USER}-xpk-${TPU_TYPE}-... # Your workload name. Need to update for different run.
-export BASE_DOCKER_IMAGE=us-central1-docker.pkg.dev/deeplearning-images/reproducibility/pytorch-tpu-mixtral:multipod
+export BASE_DOCKER_IMAGE=us-central1-docker.pkg.dev/deeplearning-images/reproducibility/pytorch-tpu-mixtral:dropping
 export PROFILE_LOG_DIR=... # GCS bucket to store profile in form of gs://...
 export HF_TOKEN=... # Add your own Hugging face token to download model
