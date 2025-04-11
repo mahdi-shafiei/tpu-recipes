16cc303f55e9dfb95d0e9a442c5ee78450ad190f
Raymond Zou <raymondzou@google.com>
2025-04-11 16:35:45 +0000

    Update jax stable stack image to 0.5.2

--- microbenchmarks/trillium/collectives/collectives-2xv6e-256.sh
+++ microbenchmarks/trillium/collectives/collectives-2xv6e-256.sh
@@ -5,5 +5,5 @@ python3 ~/xpk/xpk.py workload create \
     --device-type=v6e-256 \
     --command="git clone https://github.com/AI-Hypercomputer/accelerator-microbenchmarks.git && cd accelerator-microbenchmarks && git checkout trillium-collectives && pip install -r requirements.txt && echo '4096 41943040 314572800' > /proc/sys/net/ipv4/tcp_rmem && export LIBTPU_INIT_ARGS='--megascale_grpc_premap_memory_bytes=17179869184 --xla_tpu_enable_sunk_dcn_allreduce_done_with_host_reduction=true' && python src/run_benchmark.py --config=configs/2x_v6e_256.yaml" \
     --num-slices=2 \
-    --docker-image=us-docker.pkg.dev/cloud-tpu-images/jax-stable-stack/tpu:jax0.4.37-rev1 \
+    --docker-image=us-docker.pkg.dev/cloud-tpu-images/jax-stable-stack/tpu:jax0.5.2-rev1 \
     --workload=${WORKLOAD_NAME}
