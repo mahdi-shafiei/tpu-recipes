6472c996cad7ef60454df09e97e9f032cecba065
Carlos Bustamante Horta <carlosbus@google.com>
2025-10-18 16:35:12 +0000

    Add recipes for Gemma3-12B on v6e
    Gemma3-12B on v6e-256
    Gemma3-12B on 2 slices of v6e-256
    Gemma3-12B on 4 slices of v6e-256

---
+++ training/trillium/Gemma3-12B-MaxText/2x-v6e-256/gemma3-12B-2xv6e-256.sh
@@ -0,0 +1,10 @@
+# Run this command from the MaxText root directory using the setup described in the README.
+python3 -m benchmarks.benchmark_runner xpk \
+    --project=$PROJECT \
+    --zone=$ZONE \
+    --device_type=v6e-256 \
+    --num_slices=2  \
+    --cluster_name=${CLUSTER_NAME} \
+    --base_output_directory=${OUTPUT_DIR} \
+    --model_name="gemma3_12b_32768_2x_v6e256" \
+    --base_docker_image=maxtext_base_image
