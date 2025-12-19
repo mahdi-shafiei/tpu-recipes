6d81694e2108382553a8065b1c3135dd355cb9ef
Ankit Kinra <1037624+ankitkinra@users.noreply.github.com>
2025-12-19 07:34:54 +0000

    Update llama405b-8k-256-bf16 recipe

--- training/ironwood/llama3.1-405b/8k-fp8-tpu7x-4x8x8/run_recipe.sh
+++ training/ironwood/llama3.1-405b/8k-fp8-tpu7x-4x8x8/run_recipe.sh
@@ -13,7 +13,7 @@ source "${UV_VENV_PATH}/bin/activate"
 # Check if xpk is installed in the venv
 if ! pip show xpk &> /dev/null; then
     echo "xpk not found in the virtual environment. Please install it by running:"
-    echo "pip install xpk==0.16.0"
+    echo "pip install xpk==0.16.1"
     exit 1
 fi
 # --- End Environment Setup ---
@@ -29,7 +29,7 @@ export CLUSTER_NAME=""
 export ZONE=""
 export BASE_OUTPUT_DIR=""
 export WORKLOAD_IMAGE=""
-export WORKLOAD_NAME="$(printf "%.26s" "${USER//_/-}-llama3-1-405b-8192-fp8-4x4x4")-$(date +%Y%m%d-%H%M)"
+export WORKLOAD_NAME="$(printf "%.26s" "${USER//_/-}-llama3-1-405b-8192-4x8x8")-$(date +%Y%m%d-%H%M)"
 
 # XLA Flags
 XLA_FLAGS=" \
@@ -64,7 +64,6 @@ decoder_layer_input=offload \
 ici_fsdp_parallelism=-1 \
 dataset_type=synthetic \
 opt_type=adamw \
-quantization=fp8_full \
 mu_dtype=bfloat16 \
 use_tokamax_splash=True \
 use_max_logit_estimate=30 \
@@ -80,11 +79,11 @@ sa_v_layout=SEQ_MINOR \
 attention=flash \
 sa_use_fused_bwd_kernel=True \
 max_target_length=8192 \
-use_qwix_quantization=True \
 profiler=xplane \
 steps=30 \
 base_output_directory=${BASE_OUTPUT_DIR} \
-run_name=${WORKLOAD_NAME}"
+run_name=${WORKLOAD_NAME} \
+output_dir=${BASE_OUTPUT_DIR}"
 
 xpk workload create \
   --cluster=$CLUSTER_NAME \
